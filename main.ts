fcc055e4c3b8c57e89feec2bf69c640f652ec281
elizabeth-li-CV <123103091+elizabeth-li-CV@users.noreply.github.com>
2023-03-21 14:48:14 -0700

    Update main.blocks, main.ts, tilemap.g.jres, tilemap.g.ts

--- main.ts
+++ main.ts
@@ -17,38 +17,22 @@ let mySprite = sprites.create(img`
     . . . . . . . . . c c c c c . . 
     `, SpriteKind.Player)
 let mySprite2 = sprites.create(img`
-    ..............bbbbbbb...........
-    ...........bb66663333baa........
-    .........bb3367776333663aa......
-    ........b33333888333389633aa....
-    .......b3333333333333389633aa...
-    ......b34443333333333338633bae..
-    .....b3455433333333334443333ae..
-    ....b33322333dddd3333455233daee.
-    ...b3d333333dd3bbbb33322333dabe.
-    ..b3d333333d3bb33bb33333333da4e.
-    ..bd33333333b33aab3333333223a4ee
-    .b3d3663333b33aab33366332442b4ee
-    .bd3b983333a3aa3333387633ee3b4ee
-    .bd6983333baaa333333387633bb4bee
-    b3d6833333bba333333333863ba44ebe
-    bdd3333333bb3333333333333a44bebe
-    add666633333322333366333ba44bbbe
-    ad67776333332442336983d3a444b4e.
-    add888b333333ee3369833d3a44b44e.
-    add333333333333336833d3a444b4e..
-    a3dd3333344433333dddd3a444b44e..
-    ab33ddd325543333dd33aa444b44e...
-    .eabb3dd32233333baaa4444b44e....
-    .ebabb3d333d33baa444443b44e.....
-    ..ebaab3ddd3aaa4444433b44e......
-    ..eebbaab33a44444333b444e.......
-    ...eeebbaab444b333b4444e........
-    ....ebeeebbbbbbbb4444ee.........
-    .....eebbbb44444444ee...........
-    .......eeebbb444eee.............
-    ..........eeeeee................
-    ................................
+    . . . . . . b b b b a a . . . . 
+    . . . . b b d d d 3 3 3 a a . . 
+    . . . b d d d 3 3 3 3 3 3 a a . 
+    . . b d d 3 3 3 3 3 3 3 3 3 a . 
+    . b 3 d 3 3 3 3 3 b 3 3 3 3 a b 
+    . b 3 3 3 3 3 a a 3 3 3 3 3 a b 
+    b 3 3 3 3 3 a a 3 3 3 3 d a 4 b 
+    b 3 3 3 3 b a 3 3 3 3 3 d a 4 b 
+    b 3 3 3 3 3 3 3 3 3 3 d a 4 4 e 
+    a 3 3 3 3 3 3 3 3 3 d a 4 4 4 e 
+    a 3 3 3 3 3 3 3 d d a 4 4 4 e . 
+    a a 3 3 3 d d d a a 4 4 4 e e . 
+    . e a a a a a a 4 4 4 4 e e . . 
+    . . e e b b 4 4 4 4 b e e . . . 
+    . . . e e e e e e e e . . . . . 
+    . . . . . . . . . . . . . . . . 
     `, SpriteKind.Player)
 controller.player1.moveSprite(mySprite)
 controller.player2.moveSprite(mySprite2)
