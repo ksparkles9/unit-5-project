f57c858050aae9367ff49ab4b81af2d230362652
elizabeth-li-CV <123103091+elizabeth-li-CV@users.noreply.github.com>
2023-03-23 09:19:52 -0700

    brown stair overlap with Sprites (with booleans), start conditions

--- main.ts
+++ main.ts
@@ -1,8 +1,26 @@
 scene.onOverlapTile(SpriteKind.Player, sprites.dungeon.stairLadder, function (sprite, location) {
-    sprites.destroy(sprite)
+    if (!(canPass)) {
+        // When "canPass" is false the Cherry Sprite cannot go through the brown stair blocks because it will get destroyed.
+        sprites.destroy(sprite, effects.fire, 200)
+    }
 })
-// Have tile overlaps for certain blocks for Player 1 and Player 2.
-let mySprite = sprites.create(img`
+function startConditions (mySprite: Sprite, mySprite2: Sprite) {
+    controller.player1.moveSprite(mySprite)
+    controller.player2.moveSprite(mySprite2)
+    tiles.setCurrentTilemap(tilemap`level1`)
+    tiles.placeOnRandomTile(mySprite, sprites.dungeon.darkGroundNorthWest1)
+    tiles.placeOnRandomTile(mySprite2, sprites.dungeon.darkGroundNorthWest1)
+    scene.cameraFollowSprite(mySprite)
+    canPass = false
+}
+// Sets "canPass" to true which lets the Cherry Sprite pass by safely.
+scene.onOverlapTile(SpriteKind.Enemy, sprites.dungeon.floorDarkDiamond, function (sprite, location) {
+    canPass = true
+    sprite.sayText("Safe For Cherry To Pass Now!", 500, false)
+    sprite.startEffect(effects.confetti, 500)
+})
+let canPass = false
+startConditions(sprites.create(img`
     . . . . . . . . . . . 6 6 6 6 6 
     . . . . . . . . . 6 6 7 7 7 7 8 
     . . . . . . 8 8 8 7 7 8 8 6 8 8 
@@ -19,8 +37,7 @@ let mySprite = sprites.create(img`
     . . . . . . . c e e e e e e 2 c 
     . . . . . . . . c e 2 2 2 2 c . 
     . . . . . . . . . c c c c c . . 
-    `, SpriteKind.Player)
-let mySprite2 = sprites.create(img`
+    `, SpriteKind.Player), sprites.create(img`
     . . . . . . b b b b a a . . . . 
     . . . . b b d d d 3 3 3 a a . . 
     . . . b d d d 3 3 3 3 3 3 a a . 
@@ -37,10 +54,4 @@ let mySprite2 = sprites.create(img`
     . . e e b b 4 4 4 4 b e e . . . 
     . . . e e e e e e e e . . . . . 
     . . . . . . . . . . . . . . . . 
-    `, SpriteKind.Enemy)
-controller.player1.moveSprite(mySprite)
-controller.player2.moveSprite(mySprite2)
-tiles.setCurrentTilemap(tilemap`level1`)
-tiles.placeOnRandomTile(mySprite, sprites.dungeon.darkGroundNorthWest1)
-tiles.placeOnRandomTile(mySprite2, sprites.dungeon.darkGroundNorthWest1)
-scene.cameraFollowSprite(mySprite)
+    `, SpriteKind.Enemy))
